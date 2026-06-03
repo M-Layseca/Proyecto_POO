d7ed6823186a5b013ec39f496bc1300a44b70f07
Diego-Villanueva-Moreno <dvillanueva@usm.cl>
2026-06-02 21:51:12 -0400

    Delete pruebas de fx directory

--- pruebas de fx/src/main/java/com/example/pruebas_de_fx/Hechizos.java
+++
@@ -1,7 +0,0 @@
-package com.example.pruebas_de_fx;
-
-public interface Hechizos { // Debe ser 'interface'
-    String getNombreHechizo();
-    int getCosteMana();
-    void aplicarEfecto(Jugador lanzador, Entidad objetivo); // Usamos Entidad como tipo
-}
